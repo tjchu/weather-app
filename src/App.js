8caa39ba8a8833c367aae98a13f293f7e3836d4d
Terry Chu <terrychu2002@gmail.com>
2024-01-04 16:58:26 -0800

    update

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
         setLong(position.coords.longitude);
       });
 
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
       });
     }
     fetchData();
