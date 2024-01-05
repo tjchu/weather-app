ac1cef2045c1ac0e35f212f3baf3704994e3ea3f
Terry Chu <terrychu2002@gmail.com>
2024-01-04 22:01:39 -0800

    update

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.css';
+import moment from 'moment';
 import { Card } from 'semantic-ui-react'
+
 
 const CardExampleCard = ({weatherData}) => (
   <Card>
@@ -10,6 +12,9 @@ const CardExampleCard = ({weatherData}) => (
         <p>Sunrise: {weatherData.sys.sunrise}</p>
         <p>Sunset: {weatherData.sys.sunset}</p>
         <p>Description: {weatherData.weather[0].description}</p>
+        <p>Humidity: {weatherData.main.humidity} %</p>
+        <p>Day: {moment().format('dddd')}</p>
+        <p>Date: {moment().format('LL')}</p>
     </Card.Content>
   </Card>
 )
